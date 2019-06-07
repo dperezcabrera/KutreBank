9e518b1b64ee50b714a1c094c6329d9c5a1ff429
dperezcabrera <dperezcabrera@gmail.com>
2019-06-07 09:24:59 +0200

    Refactorizar nicializacion para demo.

--- src/main/java/com/github/dperezcabrera/bank/Initializer.java
+++ src/main/java/com/github/dperezcabrera/bank/Initializer.java
@@ -1,8 +1,10 @@
 package com.github.dperezcabrera.bank;
 
+import com.github.dperezcabrera.bank.architecture.auth.entities.Code;
 import com.github.dperezcabrera.bank.architecture.auth.entities.User;
 import com.github.dperezcabrera.bank.architecture.auth.repositories.CodeRepository;
 import com.github.dperezcabrera.bank.architecture.auth.repositories.UserRepository;
+import com.github.dperezcabrera.bank.architecture.auth.services.FeatureService;
 import com.github.dperezcabrera.bank.architecture.auth.services.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -16,12 +18,20 @@ import lombok.AllArgsConstructor;
 public class Initializer implements CommandLineRunner {
 
     private UserService userService;
+    private FeatureService featureService;
     private UserRepository userRepository;
     private CodeRepository codeRepository;
 
     @Override
     @Transactional
     public void run(String... strings) throws Exception {
-        userRepository.save(new User(0L, "admin", "***********", 0, false));
+        userRepository.save(new User(0L, "admin", "******", 0, false));
+        for (int i = 1; i <= 27; i++) {
+            codeRepository.save(new Code(Integer.toString(i), null, 10000));
+        }
+        for (int i = 28; i <= 50; i++) {
+            codeRepository.save(new Code(Integer.toString(i), null, 0));
+        }
+        featureService.setActiveFeatures(featureService.getAllFeatures());
     }
 }
